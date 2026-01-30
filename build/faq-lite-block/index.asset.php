b510530a93b98136c02fedafadb240b08c0c5bdd
HlibPanchenko <glebpanchenk7@gmail.com>
2026-01-30 18:00:37 +0200

    Feat: initial commit

---
+++ build/faq-lite-block/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '9be26efccb2a08aa87e6');
---
+++ build/faq-lite-block/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'a3b2356227a9b6852bed');
